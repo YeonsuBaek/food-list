67e262806a6053e8d0fd8e04b21e92efe6c05031
yeonsuBeak <soothbugger@gmail.com>
2021-08-13 16:53:35 +0900

    🚧 Handle search modal toggle interaction

--- js/search.js
+++ js/search.js
@@ -7,3 +7,11 @@ document.getElementById('filter-close').addEventListener('click', function () {
   document.getElementById('filter-sidebar').classList.remove('is-active')
   document.getElementById('overlay').classList.remove('is-active')
 })
+
+document.getElementById('search-button').addEventListener('click', function () {
+  document.getElementById('search-modal').classList.add('is-active')
+})
+
+document.getElementById('close-button').addEventListener('click', function () {
+  document.getElementById('search-modal').classList.remove('is-active')
+})
