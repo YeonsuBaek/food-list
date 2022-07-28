f38e0d45df8259afbd0fec016ff7c4ec1e4d8af6
yeonsuBeak <soothbugger@gmail.com>
2022-07-28 10:36:20 +0900

    🚧 Implement toggling store-tab-buttons

--- js/clickEvent.js
+++ js/clickEvent.js
@@ -16,3 +16,21 @@ document.getElementById('overlay').addEventListener('click', function () {
   document.getElementById('overlay').classList.remove('is-active')
   document.getElementById('filter-sidebar').classList.remove('is-active')
 })
+
+document
+  .querySelector('.store-review-button')
+  .addEventListener('click', function () {
+    document.querySelector('.store-menu-group').classList.remove('is-active')
+    document.querySelector('.store-menu-button').classList.remove('is-active')
+    document.querySelector('.store-review-group').classList.add('is-active')
+    document.querySelector('.store-review-button').classList.add('is-active')
+  })
+
+document
+  .querySelector('.store-menu-button')
+  .addEventListener('click', function () {
+    document.querySelector('.store-review-group').classList.remove('is-active')
+    document.querySelector('.store-review-button').classList.remove('is-active')
+    document.querySelector('.store-menu-group').classList.add('is-active')
+    document.querySelector('.store-menu-button').classList.add('is-active')
+  })
