6aa977d95c9602715bacf420f9707a1c88761e6e
yeonsuBeak <soothbugger@gmail.com>
2022-08-02 15:51:14 +0900

    ♻️ Refactor the search-button from search.js to sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -20,3 +20,11 @@ document.querySelector('.filter-close').addEventListener('click', function () {
   document.querySelector('.filter-sidebar').classList.remove('is-active')
   document.querySelector('.overlay').classList.remove('is-active')
 })
+
+document.querySelector('.search-button').addEventListener('click', function () {
+  document.querySelector('.search-modal').classList.add('is-active')
+})
+
+document.querySelector('.close-button').addEventListener('click', function () {
+  document.querySelector('.search-modal').classList.remove('is-active')
+})
