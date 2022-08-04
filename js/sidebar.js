405eac253dc95890fec3113d7c6c69c88d35f370
yeonsuBeak <soothbugger@gmail.com>
2022-08-04 12:30:11 +0900

    🔥 Delete the filter sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -11,16 +11,6 @@ document.querySelector('.overlay').addEventListener('click', function () {
   document.querySelector('.filter-sidebar').classList.remove('is-active')
 })
 
-document.querySelector('.filter-button').addEventListener('click', function () {
-  document.querySelector('.filter-sidebar').classList.add('is-active')
-  document.querySelector('.overlay').classList.add('is-active')
-})
-
-document.querySelector('.filter-close').addEventListener('click', function () {
-  document.querySelector('.filter-sidebar').classList.remove('is-active')
-  document.querySelector('.overlay').classList.remove('is-active')
-})
-
 document.querySelector('.search-button').addEventListener('click', function () {
   document.querySelector('.search-modal').classList.add('is-active')
 })
