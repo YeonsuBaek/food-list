07210112963c1be73190b4dbef8906399bc0d6e0
yeonsuBeak <soothbugger@gmail.com>
2022-08-06 14:00:27 +0900

    🚸 Improve the filter menu toggling

--- js/filter.js
+++ js/filter.js
@@ -1,17 +1,33 @@
 const filterButtonList = document.querySelectorAll('.filter-toggle')
 const overlay = document.querySelector('.overlay')
+const responsive = window.matchMedia('screen and (max-width: 768px')
 
 function toggleFilterMenu() {
   const filter = this.parentNode
+  const filterOpened = document.querySelector('.filter.is-open')
 
-  filter.classList.add('is-open')
-  overlay.classList.add('is-active')
+  if (filterOpened) {
+    console.log(1)
+    filterOpened.classList.remove('is-open')
+  }
+
+  if (filter == filterOpened) {
+    console.log(2)
+    filter.classList.remove('is-open')
+  } else {
+    console.log(3)
+    filter.classList.add('is-open')
+  }
+
+  if (responsive.matches) {
+    overlay.classList.add('is-active')
+  }
 }
 
 function closeFilterMenu() {
-  const filter = document.querySelector('.filter.is-open')
+  const filterOpened = document.querySelector('.filter.is-open')
 
-  filter.classList.remove('is-open')
+  filterOpened.classList.remove('is-open')
   overlay.classList.remove('is-active')
 }
 
