5d7b76f586925b512e459e8aada55dac403c8d69
yeonsuBeak <soothbugger@gmail.com>
2022-08-11 11:35:50 +0900

    ♻️ Refactor toggling the search modal

--- js/sidebar.js
+++ js/sidebar.js
@@ -11,10 +11,20 @@ document.querySelector('.overlay').addEventListener('click', function () {
   document.querySelector('.filter-sidebar').classList.remove('is-active')
 })
 
-document.querySelector('.search-button').addEventListener('click', function () {
-  document.querySelector('.search-modal').classList.add('is-active')
-})
+const searchButton = document.querySelector('.search-button')
+const closeButton = document.querySelector('.close-button')
+const searchModal = document.querySelector('.search-modal')
+const overlay = document.querySelector('.overlay')
 
-document.querySelector('.close-button').addEventListener('click', function () {
-  document.querySelector('.search-modal').classList.remove('is-active')
-})
+function openSearchModal() {
+  searchModal.classList.add('is-active')
+  overlay.classList.add('is-active')
+}
+
+function closeSearchModal() {
+  searchModal.classList.remove('is-active')
+  overlay.classList.remove('is-active')
+}
+
+searchButton.addEventListener('click', openSearchModal)
+closeButton.addEventListener('click', closeSearchModal)
